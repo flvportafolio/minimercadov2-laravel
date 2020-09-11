a071634ec68accf7a63436fe9ff256e65e6a9772
flvportafolio <fabiolv2019@hotmail.com>
2020-09-11 15:35:49 -0400

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+       
+        <h3>Laravel v{{ Illuminate\Foundation\Application::VERSION }}</h3>
+                   
+    </body>
+</html>
