0b96fb3c52b80979a2ae7b660097550fff373a6a
flvportafolio <fabiolv2019@hotmail.com>
2020-09-11 16:15:37 -0400

    se crearon las migraciones,modelos y controladores

---
+++ app/Http/Controllers/EmpleadoUsuarioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmpleadoUsuario;
+use Illuminate\Http\Request;
+
+class EmpleadoUsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EmpleadoUsuario  $empleadoUsuario
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EmpleadoUsuario $empleadoUsuario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EmpleadoUsuario  $empleadoUsuario
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EmpleadoUsuario $empleadoUsuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EmpleadoUsuario  $empleadoUsuario
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EmpleadoUsuario $empleadoUsuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EmpleadoUsuario  $empleadoUsuario
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EmpleadoUsuario $empleadoUsuario)
+    {
+        //
+    }
+}
